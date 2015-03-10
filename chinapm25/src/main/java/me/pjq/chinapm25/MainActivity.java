38a0a6314fe0df44b2b1934cfbc71d3b042ed863
Jianqing.Peng <Jianqing.Peng@ef.com>
2015-03-10 11:42:04 +0800

    add full screen

--- chinapm25/src/main/java/me/pjq/chinapm25/MainActivity.java
+++ chinapm25/src/main/java/me/pjq/chinapm25/MainActivity.java
@@ -3,6 +3,7 @@ package me.pjq.chinapm25;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
 import android.view.WindowManager;
 
 public class MainActivity extends FragmentActivity {
@@ -22,4 +23,17 @@ public class MainActivity extends FragmentActivity {
         }
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 }
